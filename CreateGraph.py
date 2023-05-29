22e85bf73ad01088d6e54552edef6cb924ef656a
pleijan <pierre.tom@outlook.com>
2023-05-29 13:59:47 -0400

    add graph réseaux

---
+++ CreateGraph.py
@@ -0,0 +1,27 @@
+import string
+
+from pyvis.network import Network
+import network_scan
+
+
+def createGraph(type, IP):
+    net = Network()
+    net.add_node("Routeur", color="#33FF6C")  # Ajoute un nœud pour le routeur
+
+    # Récupère la liste des ordinateurs connectés
+    clients_list = network_scan.network_scan(IP)
+
+    # Ajoute un nœud pour chaque ordinateur connecté
+    for client in clients_list:
+        net.add_node(client["ip"], label=client["ip"] + "\n" + client["mac"])
+
+    # Ajoute une connexion entre le routeur et chaque ordinateur connecté
+    for client in clients_list:
+        net.add_edge("Routeur", client["ip"])
+
+    if type == 1:
+        net.height = "300px"
+    else:
+        net.height = "700px"
+    # Rendre le graphique Pyvis dans un fichier HTML
+    net.write_html("templates/mygraph.html")
--- app.py
+++ app.py
@@ -1,53 +1,23 @@
 from flask import Flask, render_template
-from pyvis.network import Network
-import network_scan
+
+import CreatePlot
+import CreateGraph
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    net = Network()
-    net.add_node("Routeur",color="#33FF6C")  # Ajoute un nœud pour le routeur
 
-    # Récupère la liste des ordinateurs connectés
-    clients_list = network_scan.network_scan("192.168.1.0/24")
 
-    # Ajoute un nœud pour chaque ordinateur connecté
-    for client in clients_list:
-        net.add_node(client["ip"], label=client["ip"] + "\n" + client["mac"])
-
-    # Ajoute une connexion entre le routeur et chaque ordinateur connecté
-    for client in clients_list:
-        net.add_edge("Routeur", client["ip"])
-
-    net.height = "300px"
-    # Rendre le graphique Pyvis dans un fichier HTML
-    net.write_html("templates/mygraph.html")
-
-    # Renvoyer le contenu HTML dans une vue Flask
+    CreateGraph.createGraph(1, "172.20.224.0")
+    CreatePlot.createPlot()
     return render_template("index.html")
 
 @app.route('/reseaux/')
 def reseaux():
-    # Créer un objet Network Pyvis
-    net = Network()
-    net.add_node("Routeur",color="#33FF6C")  # Ajoute un nœud pour le routeur
 
-    # Récupère la liste des ordinateurs connectés
-    clients_list = network_scan.network_scan("192.168.1.0/24")
-
-    # Ajoute un nœud pour chaque ordinateur connecté
-    for client in clients_list:
-        net.add_node(client["ip"], label=client["ip"] + "\n" + client["mac"])
-
-    # Ajoute une connexion entre le routeur et chaque ordinateur connecté
-    for client in clients_list:
-        net.add_edge("Routeur", client["ip"])
-
-    net.height = "700px"
-    # Rendre le graphique Pyvis dans un fichier HTML
-    net.write_html("templates/mygraph2.html")
-
+    CreateGraph.createGraph(0, "172.20.224.0")
     return render_template('reseaux.html')
 
 @app.route('/tableau/')
@@ -56,6 +26,7 @@ def tableau():
 
 @app.route('/graph/')
 def graph():
+    CreatePlot.createPlot()
     return render_template('graph.html')
 
 @app.route('/carte/')
