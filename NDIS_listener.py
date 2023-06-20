c5a9df4ddd6922de786c19d185dca5c5b8ec104f
enzo-hmdy <enzo.hmdy@gmail.com>
2023-06-20 17:03:31 -0400

    modif listener

--- NDIS_listener.py
+++ NDIS_listener.py
@@ -1,12 +1,20 @@
 import socket
 import os 
 
+
+
 def listener():
+
+    #check if file data.txt exists
+    if not os.path.exists("data.txt"):
+        # create a new file if it does not exist
+        open("data.txt", "w+")
+    
     # Create a UDP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     # Bind the socket to the port
-    server_address = ('192.168.1.206', 444)
+    server_address = ('192.168.1.206',514 )
     print('starting up on {} port {}'.format(*server_address))
     sock.bind(server_address)
 
@@ -19,10 +27,10 @@ def listener():
 
         if data:
             # open a new file in current folder to write to the data
-            with open("data.txt", "wb") as f:
+            with open("data.txt", "a") as f:
                 f.write(data)
             # close the file
-            
+
 
             
 
